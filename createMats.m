4d26838c4eebcc9dad0b75c08fa8a3c409ca14c1
Dean <roros2@illinois.edu>
2014-12-06 16:52:56 -0600

    loadtrim function and create file script

---
+++ createMats.m
@@ -0,0 +1,5 @@
+fLoadTrim('../final-data/Basketball/Arm/Jump Shot','jumpShotFeaturesArm.mat');
+fLoadTrim('../final-data/Basketball/One-Hand Pass/Jump Shot','oneHandPassFeaturesArm.mat');
+fLoadTrim('../final-data/Basketball/Wrist/One-Hand','oneHandPassFeaturesWrist.mat');
+fLoadTrim('../final-data/Basketball/Wrist/Pass','oneHandPassFeaturesWrist.mat');
+
